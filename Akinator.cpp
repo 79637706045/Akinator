52144edfee11b4da47f3b867cc4f40d1b8a8a3ea
79637706045 <36453460+79637706045@users.noreply.github.com>
2018-02-14 01:30:00 +0300

    Update Akinator.cpp

--- Akinator.cpp
+++ Akinator.cpp
@@ -1,14 +1,21 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
 
+typedef struct my_strucy1{
+	string value;
+	my_strucy1* left, right;
+}tree_el;
 
 
+void info(){
+	printf("Добро пожаловать.");
+}
 
 int main(){
-	
-	printf("Hello world.\n");
-	
-	printf("Hello world again!");
-	
+	info();
+
+
 	return 0;
 }
